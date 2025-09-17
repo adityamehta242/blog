e341463b014d49d19753cee6d9dc94d3c7947dfc
adityamehta242 <adityamehta242@gmail.com>
2025-09-18 01:06:43 +0530

    complete entity

--- backend/src/main/java/space/personalshowcase/blog/domain/entities/Category.java
+++ backend/src/main/java/space/personalshowcase/blog/domain/entities/Category.java
@@ -1,5 +1,8 @@
 package space.personalshowcase.blog.domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -7,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,6 +34,23 @@ public class Category {
 	@Column(nullable = false , unique = true)
 	private String name;
 	
-	
-	
+	@OneToMany(mappedBy = "category")
+	private List<Post> posts = new ArrayList<>();
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
 }
--- backend/src/main/java/space/personalshowcase/blog/domain/entities/Tag.java
+++ backend/src/main/java/space/personalshowcase/blog/domain/entities/Tag.java
@@ -1,6 +1,8 @@
 package space.personalshowcase.blog.domain.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -8,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,6 +33,9 @@ public class Tag {
 	
 	@Column(nullable = false , unique = true)
 	private String name;
+	
+	@ManyToMany(mappedBy = "tags")
+	private Set<Post> posts = new HashSet<>();
 
 	@Override
 	public int hashCode() {
