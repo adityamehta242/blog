199715ac9dca798fbcc152e9cac08c6c218ce4f5
adityamehta242 <adityamehta242@gmail.com>
2025-09-18 22:19:59 +0530

    complete Repository implementation

---
+++ backend/src/main/java/space/personalshowcase/blog/repositories/PostRepository.java
@@ -0,0 +1,13 @@
+package space.personalshowcase.blog.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import space.personalshowcase.blog.domain.entities.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+}
---
+++ backend/src/main/java/space/personalshowcase/blog/repositories/TagRepository.java
@@ -0,0 +1,13 @@
+package space.personalshowcase.blog.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import space.personalshowcase.blog.domain.entities.Tag;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+}
