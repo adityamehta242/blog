f910d5d4bcfa3f0316f56d83e0909dbaab685c79
adityamehta242 <adityamehta242@gmail.com>
2025-09-20 01:41:43 +0530

    add ExceptionHandler

---
+++ backend/src/main/java/space/personalshowcase/blog/controllers/ErrorController.java
@@ -0,0 +1,44 @@
+package space.personalshowcase.blog.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import space.personalshowcase.blog.domain.dtos.ApiErrorResponse;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiErrorResponse> handleException(Exception ex)
+	{
+		log.error("Caught exception " + ex);
+		
+		ApiErrorResponse error = ApiErrorResponse.builder()
+				.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+				.message("An unexpected error occur")
+				.build();
+		
+		return new  ResponseEntity<>(error , HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex)
+	{
+		log.error("Caught exception " + ex);
+		
+		ApiErrorResponse error = ApiErrorResponse.builder()
+				.status(HttpStatus.BAD_REQUEST.value())
+				.message(ex.getMessage())
+				.build();
+		
+		return new  ResponseEntity<>(error , HttpStatus.BAD_REQUEST);
+	}
+	
+	
+}     
