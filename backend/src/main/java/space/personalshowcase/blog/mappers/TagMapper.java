340751f4db81ce4ba3d11b3c2141a586a0f1a508
adityamehta242 <adityamehta242@gmail.com>
2025-09-23 00:00:49 +0530

    add tagMapper

---
+++ backend/src/main/java/space/personalshowcase/blog/mappers/TagMapper.java
@@ -0,0 +1,31 @@
+package space.personalshowcase.blog.mappers;
+
+import java.util.List;
+import java.util.Set;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import space.personalshowcase.blog.domain.PostStatus;
+import space.personalshowcase.blog.domain.dtos.TagResponseDto;
+import space.personalshowcase.blog.domain.entities.Post;
+import space.personalshowcase.blog.domain.entities.Tag;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TagMapper {
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    TagResponseDto toTagResponseDto(Tag tag);
+
+    @Named("calculatePostCount")
+    default Integer calculatePostCount(Set<Post> posts) {
+        if (posts == null) {
+            return 0;
+        }
+        return (int) posts.stream()
+            .filter(post -> PostStatus.PUBLISHED.equals(post.getPostStatus()))
+            .count();
+    }
+}
